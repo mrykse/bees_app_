cb6de128fa406446a74377abe205313b2e14c46f
mry_kse <meryem.kose@efrei.net>
2024-02-25 12:40:09 +0100

    change post address to vercel

--- components/Signaler/page.tsx
+++ components/Signaler/page.tsx
@@ -65,7 +65,7 @@ export const Support = () => {
                 status_inquiry: "non_consulte"
             };
 
-            const response = await fetch('http://localhost:3000/api/interventions', {
+            const response = await fetch('https://sos-abbeilles.vercel.app/api/interventions', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
