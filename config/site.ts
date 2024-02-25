5ceb8d46c02e804c31e5e42cf1a8dc80dc02fd52
mry_kse <meryem.kose@efrei.net>
2024-02-25 12:41:13 +0100

    change site name

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: "Next.js + NextUI",
+	name: "ACFT94 - SOS abeilles ",
 	description: "Make beautiful websites regardless of your design experience.",
 	navItems: [
 		{
